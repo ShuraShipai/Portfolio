e5da4dc44173672f6e4346b91a89486bb4550f30
ShuraShipai <shura.shipai2005@gmail.com>
2024-11-27 16:18:39 +0530

    Portfoilio

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle Dark Mode (for future expansion)
+const toggleButton = document.querySelector('.toggle-button');
+toggleButton.addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+});
